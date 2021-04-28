1e106913f26118bb7b5f12658af6e7a3ded29a4a
Manoj Kukreja <mkukreja1@gmail.com>
2021-04-28 13:48:47 -0400

    batch ingestion pipeline

---
+++ project/prep/stores/sql/incremental/2/storedb_incremental_2.sql
@@ -0,0 +1,6 @@
+UPDATE [store_orders] SET [sale_price] = '47.65', [currency] = 'CAD', [order_mode] = 'EDIT', [updated_at] = CURRENT_TIMESTAMP WHERE [order_number] = 1501;
+UPDATE [store_orders] SET [sale_price] = '32.10', [currency] = 'USD', [order_mode] = 'EDIT', [updated_at] = CURRENT_TIMESTAMP WHERE [order_number] = 2345;
+UPDATE [store_orders] SET [sale_price] = '49.78', [currency] = 'USD', [order_mode] = 'EDIT', [updated_at] = CURRENT_TIMESTAMP WHERE [order_number] = 3001;
+UPDATE [store_orders] SET [sale_price] = '34.40', [currency] = 'USD', [order_mode] = 'EDIT', [updated_at] = CURRENT_TIMESTAMP WHERE [order_number] = 2234;
+UPDATE [store_orders] SET [sale_price] = '93.77', [currency] = 'CAD', [order_mode] = 'EDIT', [updated_at] = CURRENT_TIMESTAMP WHERE [order_number] = 500;
+GO
---
+++ project/prep/stores/sql/incremental/3/storedb_incremental_3.sql
@@ -0,0 +1,3 @@
+UPDATE [store_orders] SET [sale_price] = '47.65', [currency] = 'CAD', [order_mode] = 'DELETE', [updated_at] = CURRENT_TIMESTAMP WHERE [order_number] = 1501;
+UPDATE [store_orders] SET [sale_price] = '32.10', [currency] = 'USD', [order_mode] = 'DELETE', [updated_at] = CURRENT_TIMESTAMP WHERE [order_number] = 2345;
+GO
