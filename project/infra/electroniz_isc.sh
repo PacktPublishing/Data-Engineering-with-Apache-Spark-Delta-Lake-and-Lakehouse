985d08ca9a34699259961d197a70efcf1347acd1
Manoj Kukreja <mkukreja1@gmail.com>
2021-07-26 14:56:40 -0400

    infra files

--- project/infra/electroniz_isc.sh
+++ project/infra/electroniz_isc.sh
@@ -1,7 +1,8 @@
 ####################################################################################################################################################
 # Usage:
 # $ sh ~/Data-Engineering-with-Apache-Spark-Delta-Lake-and-Lakehouse/project/infra/electroniz_isc.sh IACRESOURCEGROUPNAME=elz_iac RESOURCEGROUPNAME=elz_prod LOCATION=eastus \
-#   FILE_PATH=~/Data-Engineering-with-Apache-Spark-Delta-Lake-and-Lakehouse/project/infra
+#   TEMPLATE_FILE_PATH=~/Data-Engineering-with-Apache-Spark-Delta-Lake-and-Lakehouse/project/infra \
+#   PARAMETERS_FILE_PATH=~/Data-Engineering-with-Apache-Spark-Delta-Lake-and-Lakehouse/project/infra/parameters/prod
 ####################################################################################################################################################
 
 
@@ -18,7 +19,8 @@ do
             IACRESOURCEGROUPNAME)              IACRESOURCEGROUPNAME=${VALUE} ;;
             RESOURCEGROUPNAME)                 RESOURCEGROUPNAME=${VALUE} ;;
             LOCATION)                          LOCATION=${VALUE} ;;
-            FILE_PATH)                         FILE_PATH=${VALUE} ;;
+            TEMPLATE_FILE_PATH)                TEMPLATE_FILE_PATH=${VALUE} ;;
+			PARAMETERS_FILE_PATH)              PARAMETERS_FILE_PATH=${VALUE} ;;
             *)
     esac
 
@@ -38,10 +40,9 @@ echo "End creation of Resource group: $RESOURCEGROUPNAME"
 ####################################################################################################################################################
 echo "Start creation of Lakehouse_Storage_Account"
 
-TEMPLATE_FILE=$FILE_PATH/storage_accounts/storage_accounts.json
-PARAMETERS_FILE=$FILE_PATH/storage_accounts/storage_accounts_parameters.json
+TEMPLATE_FILE=$TEMPLATE_FILE_PATH/storage_accounts/storage_accounts.json
+PARAMETERS_FILE=$PARAMETERS_FILE_PATH/storage_accounts/storage_accounts_parameters.json
 az deployment group create --name Lakehouse_Storage_Account --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE
-
 
 echo "End creation of Lakehouse_Storage_Account"
 ####################################################################################################################################################    
@@ -50,8 +51,8 @@ echo "End creation of Lakehouse_Storage_Account"
 ####################################################################################################################################################
 echo "Start creation of Stores_Azure_SQL_Database"
 
-TEMPLATE_FILE=$FILE_PATH/azure_sql/azure_sql.json
-PARAMETERS_FILE=$FILE_PATH/azure_sql/azure_sql_parameters.json
+TEMPLATE_FILE=$TEMPLATE_FILE_PATH/azure_sql/azure_sql.json
+PARAMETERS_FILE=$PARAMETERS_FILE_PATH/azure_sql/azure_sql_parameters.json
 az deployment group create --name Stores_Azure_SQL_Database --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE 
 
 echo "End creation of Stores_Azure_SQL_Database"
@@ -61,8 +62,8 @@ echo "End creation of Stores_Azure_SQL_Database"
 ####################################################################################################################################################
 echo "Start creation of Lakehouse_Event_Hub"
 
-TEMPLATE_FILE=$FILE_PATH/event_hubs/event_hub.json
-PARAMETERS_FILE=$FILE_PATH/event_hubs/event_hub_parameters.json
+TEMPLATE_FILE=$TEMPLATE_FILE_PATH/event_hubs/event_hub.json
+PARAMETERS_FILE=$PARAMETERS_FILE_PATH/event_hubs/event_hub_parameters.json
 az deployment group create --name Lakehouse_Event_Hub --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE 
 
 echo "End creation of Lakehouse_Event_Hub"
@@ -72,8 +73,8 @@ echo "End creation of Lakehouse_Event_Hub"
 ####################################################################################################################################################
 echo "Start creation of Lakehouse_Databricks"
 
-TEMPLATE_FILE=$FILE_PATH/databricks/databricks.json
-PARAMETERS_FILE=$FILE_PATH/databricks/databricks_parameters.json
+TEMPLATE_FILE=$TEMPLATE_FILE_PATH/databricks/databricks.json
+PARAMETERS_FILE=$PARAMETERS_FILE_PATH/databricks/databricks_parameters.json
 az deployment group create --name Lakehouse_Databricks --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE
 
 echo "End creation of Lakehouse_Databricks"
@@ -83,8 +84,8 @@ echo "End creation of Lakehouse_Databricks"
 ####################################################################################################################################################
 echo "Start creation of Lakehouse_DataFactory"
 
-TEMPLATE_FILE=$FILE_PATH/data_factory/data_factory.json
-PARAMETERS_FILE=$FILE_PATH/data_factory/data_factory_parameters.json
+TEMPLATE_FILE=$TEMPLATE_FILE_PATH/data_factory/data_factory.json
+PARAMETERS_FILE=$PARAMETERS_FILE_PATH/data_factory/data_factory_parameters.json
 az deployment group create --name Lakehouse_DataFactory --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE
 
 echo "End creation of Lakehouse_DataFactory"
@@ -93,8 +94,8 @@ echo "End creation of Lakehouse_DataFactory"
 ####################################################################################################################################################
 echo "Start creation of Lakehouse_Synapse"
 
-TEMPLATE_FILE=$FILE_PATH/synapse/synapse.json
-PARAMETERS_FILE=$FILE_PATH/synapse/synapse_parameters.json
+TEMPLATE_FILE=$TEMPLATE_FILE_PATH/synapse/synapse.json
+PARAMETERS_FILE=$PARAMETERS_FILE_PATH/synapse/synapse_parameters.json
 az deployment group create --name Lakehouse_Synapse --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE
 
 echo "End creation of Lakehouse_Synapse"
