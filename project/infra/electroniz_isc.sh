e97382c0455e23c449ccac5ba2c3fe4dbde9d653
Manoj Kukreja <mkukreja1@gmail.com>
2021-07-26 13:50:08 -0400

    infra files

--- project/infra/electroniz_isc.sh
+++ project/infra/electroniz_isc.sh
@@ -1,6 +1,6 @@
 ####################################################################################################################################################
 # Usage:
-# $ sh electroniz_isc.sh IACRESOURCEGROUPNAME=elz_iac RESOURCEGROUPNAME=elz_prod LOCATION=eastus \
+# $ sh ~/Data-Engineering-with-Apache-Spark-Delta-Lake-and-Lakehouse/project/infra/electroniz_isc.sh IACRESOURCEGROUPNAME=elz_iac RESOURCEGROUPNAME=elz_prod LOCATION=eastus \
 #   FILE_PATH=~/Data-Engineering-with-Apache-Spark-Delta-Lake-and-Lakehouse/project/infra
 ####################################################################################################################################################
 
@@ -36,68 +36,68 @@ echo "End creation of Resource group: $RESOURCEGROUPNAME"
 
 
 ####################################################################################################################################################
-echo "Start creation of Lakehouse_Storage_Account: $STORAGEACCOUNTNAME"
+echo "Start creation of Lakehouse_Storage_Account"
 
 TEMPLATE_FILE=$FILE_PATH/storage_accounts/storage_accounts.json
 PARAMETERS_FILE=$FILE_PATH/storage_accounts/storage_accounts_parameters.json
 az deployment group create --name Lakehouse_Storage_Account --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE
 
 
-echo "End creation of Lakehouse_Storage_Account: $STORAGEACCOUNTNAME"
+echo "End creation of Lakehouse_Storage_Account"
 ####################################################################################################################################################    
 
 
 ####################################################################################################################################################
-echo "Start creation of Stores_Azure_SQL_Database: $SERVERNAME"
+echo "Start creation of Stores_Azure_SQL_Database"
 
 TEMPLATE_FILE=$FILE_PATH/azure_sql/azure_sql.json
 PARAMETERS_FILE=$FILE_PATH/azure_sql/azure_sql_parameters.json
 az deployment group create --name Stores_Azure_SQL_Database --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE 
 
-echo "End creation of Stores_Azure_SQL_Database: $SERVERNAME"
+echo "End creation of Stores_Azure_SQL_Database"
 ####################################################################################################################################################    
 
 
 ####################################################################################################################################################
-echo "Start creation of Lakehouse_Event_Hub: $EVENTHUBNAMESPACE"
+echo "Start creation of Lakehouse_Event_Hub"
 
 TEMPLATE_FILE=$FILE_PATH/event_hubs/event_hub.json
-PARAMETERS_FILE=$FILE_PATH/event_hubs/event_hubs_parameters.json
+PARAMETERS_FILE=$FILE_PATH/event_hubs/event_hub_parameters.json
 az deployment group create --name Lakehouse_Event_Hub --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE 
 
-echo "End creation of Lakehouse_Event_Hub: $EVENTHUBNAMESPACE"
+echo "End creation of Lakehouse_Event_Hub"
 ####################################################################################################################################################    
 
 
 ####################################################################################################################################################
-echo "Start creation of Lakehouse_Databricks: $DATABRICKSWORKSPACENAME"
+echo "Start creation of Lakehouse_Databricks"
 
 TEMPLATE_FILE=$FILE_PATH/databricks/databricks.json
 PARAMETERS_FILE=$FILE_PATH/databricks/databricks_parameters.json
 az deployment group create --name Lakehouse_Databricks --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE
 
-echo "End creation of Lakehouse_Databricks: $DATABRICKSWORKSPACENAME"
+echo "End creation of Lakehouse_Databricks"
 ####################################################################################################################################################    
 
 
 ####################################################################################################################################################
-echo "Start creation of Lakehouse_DataFactory: $FACTORYNAME"
+echo "Start creation of Lakehouse_DataFactory"
 
 TEMPLATE_FILE=$FILE_PATH/data_factory/data_factory.json
 PARAMETERS_FILE=$FILE_PATH/data_factory/data_factory_parameters.json
 az deployment group create --name Lakehouse_DataFactory --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE
 
-echo "End creation of Lakehouse_DataFactory: $FACTORYNAME"
+echo "End creation of Lakehouse_DataFactory"
 ####################################################################################################################################################
 
 ####################################################################################################################################################
-echo "Start creation of Lakehouse_Synapse: $SYNAPSEWORKSPACENAME"
+echo "Start creation of Lakehouse_Synapse"
 
 TEMPLATE_FILE=$FILE_PATH/synapse/synapse.json
 PARAMETERS_FILE=$FILE_PATH/synapse/synapse_parameters.json
 az deployment group create --name Lakehouse_Synapse --resource-group $RESOURCEGROUPNAME --template-file $TEMPLATE_FILE --parameters $PARAMETERS_FILE
 
-echo "End creation of Lakehouse_Synapse: $SYNAPSEWORKSPACENAME"
+echo "End creation of Lakehouse_Synapse"
 ####################################################################################################################################################    
 
 
