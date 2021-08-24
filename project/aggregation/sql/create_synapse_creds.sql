2d281aaa196183cfd43b60bee99068e02271c705
Manoj Kukreja <mkukreja1@gmail.com>
2021-08-24 13:53:40 -0400

    changes to synapse stored procs

--- project/aggregation/sql/create_synapse_creds.sql
+++ project/aggregation/sql/create_synapse_creds.sql
@@ -9,42 +9,50 @@ BEGIN
     IF EXISTS (SELECT * FROM sys.external_file_formats WHERE name = @format)
         SET @sqlcmd = N'DROP EXTERNAL FILE FORMAT ' + @format;
         EXEC sp_executesql @sqlcmd
+        SET @sqlcmd=''
 		
 	IF EXISTS (SELECT * FROM sys.external_file_formats WHERE name = 'DeltaLakeFormat')
         SET @sqlcmd = N'DROP EXTERNAL FILE FORMAT DeltaLakeFormat';
         EXEC sp_executesql @sqlcmd
+        SET @sqlcmd=''
 
     IF EXISTS (SELECT * FROM sys.external_data_sources WHERE name = @extds) 
         SET @sqlcmd = N'DROP EXTERNAL DATA SOURCE ' + @extds;
         EXEC sp_executesql @sqlcmd
+        SET @sqlcmd=''
 
     IF EXISTS (SELECT * FROM sys.database_scoped_credentials WHERE name = @creds)
         SET @sqlcmd = N'DROP DATABASE SCOPED CREDENTIAL ' + @creds;
         EXEC sp_executesql @sqlcmd
+        SET @sqlcmd=''
 
     IF  EXISTS (SELECT * FROM sys.symmetric_keys) 
         DROP MASTER KEY;
     
     IF NOT EXISTS (SELECT * FROM sys.symmetric_keys)
-        CREATE MASTER KEY
+        CREATE MASTER KEY;
 
     IF NOT EXISTS (SELECT * FROM sys.database_scoped_credentials WHERE name = @creds)
         SET @sqlcmd = N'CREATE DATABASE SCOPED CREDENTIAL ' + @creds + ' WITH IDENTITY = ''SHARED ACCESS SIGNATURE'',
         SECRET = ''' + @secret + ''''
         EXEC sp_executesql @sqlcmd
+        SET @sqlcmd=''
 
     IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name =  @extds) 
         SET @sqlcmd = N'CREATE EXTERNAL DATA SOURCE ' + @extds + ' WITH (LOCATION = ''' + @location +
                        ''', CREDENTIAL = ' + @creds + ')';
         EXEC sp_executesql @sqlcmd
+        SET @sqlcmd=''
 
     IF  NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name =  @format)
         SET @sqlcmd = N'CREATE EXTERNAL FILE FORMAT ' +  @format + ' WITH (
         FORMAT_TYPE = PARQUET, DATA_COMPRESSION = ''org.apache.hadoop.io.compress.SnappyCodec'')';
         EXEC sp_executesql @sqlcmd
+        SET @sqlcmd=''
 	
 	IF  NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name =  'DeltaLakeFormat')
         SET @sqlcmd = N'CREATE EXTERNAL FILE FORMAT DeltaLakeFormat WITH (  FORMAT_TYPE = DELTA )';
         EXEC sp_executesql @sqlcmd
+        SET @sqlcmd=''
 END;
 GO
