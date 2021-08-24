1871abfdfdb125a4869039e3c932d3d3b48cf412
Manoj Kukreja <mkukreja1@gmail.com>
2021-08-24 14:11:01 -0400

    changes to synapse stored procs

--- project/aggregation/sql/create_synapse_creds.sql
+++ project/aggregation/sql/create_synapse_creds.sql
@@ -6,29 +6,7 @@ CREATE PROCEDURE [create_synapse_creds]  @format varchar(50), @extds varchar(50)
 AS
 BEGIN
     DECLARE @sqlcmd nvarchar(500)
-    IF EXISTS (SELECT * FROM sys.external_file_formats WHERE name = @format)
-        SET @sqlcmd = N'DROP EXTERNAL FILE FORMAT ' + @format;
-        EXEC sp_executesql @sqlcmd
-        SET @sqlcmd=''
-		
-	IF EXISTS (SELECT * FROM sys.external_file_formats WHERE name = 'DeltaLakeFormat')
-        SET @sqlcmd = N'DROP EXTERNAL FILE FORMAT DeltaLakeFormat';
-        EXEC sp_executesql @sqlcmd
-        SET @sqlcmd=''
-
-    IF EXISTS (SELECT * FROM sys.external_data_sources WHERE name = @extds) 
-        SET @sqlcmd = N'DROP EXTERNAL DATA SOURCE ' + @extds;
-        EXEC sp_executesql @sqlcmd
-        SET @sqlcmd=''
-
-    IF EXISTS (SELECT * FROM sys.database_scoped_credentials WHERE name = @creds)
-        SET @sqlcmd = N'DROP DATABASE SCOPED CREDENTIAL ' + @creds;
-        EXEC sp_executesql @sqlcmd
-        SET @sqlcmd=''
-
-    IF  EXISTS (SELECT * FROM sys.symmetric_keys) 
-        DROP MASTER KEY;
-    
+     
     IF NOT EXISTS (SELECT * FROM sys.symmetric_keys)
         CREATE MASTER KEY;
 
